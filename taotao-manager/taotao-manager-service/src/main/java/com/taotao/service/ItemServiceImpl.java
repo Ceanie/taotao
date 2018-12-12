f436194d8770235c476c4ed190bea0b0a6fe551a
Ceanie <45597642+Ceanie@users.noreply.github.com>
2018-12-12 22:43:02 +0800

    Tip
    Tip

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemServiceImpl.java
@@ -0,0 +1,23 @@
+package com.taotao.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Override
+	public TbItem getItemById(long itemId) {
+		TbItem item=itemMapper.selectByPrimaryKey(itemId);
+		return item;
+	}
+	
+	
+	
+}
