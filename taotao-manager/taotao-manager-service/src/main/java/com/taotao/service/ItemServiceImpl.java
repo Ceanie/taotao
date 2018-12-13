7def73edb435e51b908e190e9f88b5086332d922
Ceanie <45597642+Ceanie@users.noreply.github.com>
2018-12-14 02:47:51 +0800

    Updata
    Tip

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemServiceImpl.java
@@ -1,10 +1,16 @@
 package com.taotao.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.pojo.EasyUIDataGridResult;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
 
 @Service
 public class ItemServiceImpl implements ItemService{
@@ -16,6 +22,21 @@ public class ItemServiceImpl implements ItemService{
 	public TbItem getItemById(long itemId) {
 		TbItem item=itemMapper.selectByPrimaryKey(itemId);
 		return item;
+	}
+
+	@Override
+	public EasyUIDataGridResult getItemList(int page, int rows) {
+		//设置分页信息
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example=new TbItemExample();
+		List<TbItem> list=itemMapper.selectByExample(example);
+		//取查询结果
+		PageInfo<TbItem> pageInfo=new PageInfo<>(list);
+		EasyUIDataGridResult result=new EasyUIDataGridResult();
+		result.setTotal(pageInfo.getTotal());
+		result.setRows(list);
+		return result;
 	}
 	
 	
